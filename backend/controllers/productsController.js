df235817c51c844a74ef28af8f17b24f6125a5e9
José Mario Rivera Carranza <aperta@Mac-mini-de-Jose.local>
2023-12-19 11:31:45 -0700

    Creación de rutas, controladores, errorHandler y asyncHandler

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,18 +1,24 @@
-const getProductos = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getProductos = asyncHandler((req, res) => {
     res.status(200).json({ message: "Obtener productos" })
-}
+})
 
-const postProductos = (req, res) => {
+const postProductos = asyncHandler((req, res) => {
+    if (!req.body.texto) {
+        res.status(400)
+        throw new Error("Por favor teclea un nombre de producto")
+    }
     res.status(201).json({ message: "Crear producto" })
-}
+})
 
-const putProductos = (req, res) => {
+const putProductos = asyncHandler((req, res) => {
     res.status(200).json({ message: `Modificar el producto ${req.params.id}` })
-}
+})
 
-const deleteProductos = (req, res) => {
+const deleteProductos = asyncHandler((req, res) => {
     res.status(200).json({ message: `Eliminar el producto ${req.params.id}` })
-}
+})
 
 module.exports = {
     getProductos,
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ const { log } = require('console')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/middleware')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/productos', require('./routes/productsRoutes'))
+
+app.use(errorHandler)
 app.listen(port, () => console.log(`Ruta abierta en el puerto ${port}`))
